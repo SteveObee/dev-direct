33ca3b2e69b8d11680982b96a8bf7eaadd5240ac
SteveObee <stephenobee1@gmail.com>
2019-05-15 13:25:31 +0100

    Added post routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -251,7 +251,6 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
-
     res.status(500).json('Server Error');
   }
 });
