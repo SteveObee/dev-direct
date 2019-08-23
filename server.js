2cb27f7625d3a8d0e3f013ff9ac8ec6aad684022
SteveObee <stephenobee1@gmail.com>
2019-08-23 15:41:03 +0100

    Pre deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.statuc('client/build'));
+  app.use(express.static('client/build'));
 }
 
 app.get('*', (req, res) => {
