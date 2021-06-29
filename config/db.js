bd62d6352321eaf5dbad7af3579dab1d8d040a6e
Stephen Obee <stephenobee1@gmail.com>
2021-06-29 15:33:51 +0100

    Switches database to cloud:

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
-const config = require('config');
+const mongoose = require("mongoose");
+const config = require("config");
 
-const db = config.get('mongoURI');
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      useUnifiedTopology: true,
     });
 
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
     // Exit process with failure
